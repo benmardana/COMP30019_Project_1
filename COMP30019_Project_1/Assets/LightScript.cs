19ff61ba091373d327250c7dc2ea003ed73d64dd
aangelatos <aangelatos@student.unimelb.edu.au>
2018-09-05 17:18:21 +1000

    added sun-motion

--- COMP30019_Project_1/Assets/LightScript.cs
+++ COMP30019_Project_1/Assets/LightScript.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightScript : MonoBehaviour {
+public class LightScript : MonoBehaviour
+{
+    float timeCounter;
+    float speed;
+    float width;
+    float height;
+    public Vector4 lightColor = new Vector4(255, 255, 255, 10);
 
-	public Vector4 lightColor = new Vector4(255, 255, 255, 10);
+    // Use this for initialization
+    void Start()
+    {
+        speed = 0.25f;
+        width = 64;
+        height = 96;
+        timeCounter = 0;
+        transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
 
-	// Use this for initialization
-	void Start () {
-		transform.position = new Vector3(0.0f, 30.0f, 0.0f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+        Vector4 lightLoc = new Vector4(transform.position.x, transform.position.y, transform.position.z, 1.0f);
+        Shader.SetGlobalVector("_LightColor", lightColor);
+        // update location and give to shaders
+        Shader.SetGlobalVector("_LightPosition", lightLoc);
 
-		Vector4 lightLoc = new Vector4(transform.position.x, transform.position.y, transform.position.z, 1.0f);
-		Shader.SetGlobalVector("_LightColor", lightColor);
-		// update location and give to shaders
-		Shader.SetGlobalVector("_LightPosition", lightLoc);
-	}
+        timeCounter += Time.deltaTime * speed;
+
+        float x = 64 + Mathf.Cos(timeCounter) * width;
+        float y = Mathf.Sin(timeCounter) * height;
+        float z = x;
+
+        transform.position = new Vector3(x, y, z);
+     
+
+    }
 }
